f5f3cae087d2babd189e05461f8f162949491a22
elliedori <elliedori@gmail.com>
2016-08-05 13:35:49 -0700

    Update vampire doc with code for survey

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,81 @@
-puts "what's your name?"
-name = gets.chomp
+# define survey questions & logic
 
-puts "How old are you?"
-age = gets.chomp
+# def employee_survey
 
-puts "What year were you born?"
-birthyear = gets.chomp
+  result = "" # defining this now so it can be overwritten in case of sunshine allergy
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+  puts "What's your name?"
+  name = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp.to_i
+
+  puts "What year were you born?"
+  birthyear = gets.chomp
+
+  if age >= (2016 - birthyear.to_i)
+    correct_age = true
+  else
+    wrong_age = true
+  end
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  if gets.chomp == "y"
+    likes_garlic = true
+  else
+    hates_garlic = true
+  end
+
+  puts "Would you like to enroll in the company’s health insurance? (y/n)"
+  if gets.chomp == "y"
+    insur_yes_plz = true
+  else
+    insur_no_thx = true
+  end
+
+  allergies = ""
+  puts "Please list any allergies you have, one at a time. Type 'done' when you're finished."
+    while allergies != "done"
+      allergies = gets.chomp
+      if allergies.downcase == "sunshine"
+      result = "Definitely a vampire"
+    end
+  end
+  # runs vampire likelihood analysis
+
+  # I used separate if loops so that result would get overwritten as the program checked,
+  # instead of exiting the loop once one of the conditions was met. I did this because it
+  # specifically asked us to match these conditions in the order written. If that instruction
+  # weren't there, I would have used a regular if/elsif/else loop to check and would have 
+  # reversed the order of the logical statements to go from most strict to least strict.
+  # (I.e. so that it first checks for the matching name, then for the case where it's 
+  # the wrong age, hates garlic, and doens't want insurance, etc.)
+
+  if correct_age && (likes_garlic || insur_yes_plz)
+    result = "Probably not a vampire."
+  end
+
+  if wrong_age && (hates_garlic || insur_no_thx)
+    result = "Probably a vampire."
+  end
+
+  if wrong_age && hates_garlic && insur_no_thx
+    result = "Almost certainly a vampire."
+  end
+
+  if (name.downcase == "drake cula" || name.downcase == "tu fang") || allergies == "sunshine"
+    result = "Definitely a vampire."
+  end
+
+  if result == ""
+    result = "Results inconclusive."
+  end
+
+  puts result
+# end
+
+# prompt user to select number of employees & loop through program
+
+# puts "How many employees are you processing today?"
+# num_employees = gets.chomp.to_i
+# num_employees.times {|x| employee_survey}
