86a7507611ae74a515a2fd9f1fb6105acea19c98
elliedori <elliedori@gmail.com>
2016-08-11 17:27:13 -0700

    Add code for method that takes a block

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,16 +1,10 @@
 def bag_search
-  bag = ["wallet", "phone", "calculator", "gum", "key"]
-  puts bag
-  puts "This is right before our block"
-  yield
-  puts "Our block just ran. Wasn't it great?"
+  item1 = "wallet"
+  item2 = "keys"
+  puts "I'm about to look in your bag"
+  yield(item1, item2)
+  puts "Now I know what's in your bag."
 
 end
 
-bag_search do |item|
-  if bag.include? (item)
-    puts "You've got it in the bag!"
-  else
-    puts "Better luck next time"
-  end
-end
+bag_search {|item1, item2| puts "Your bag has #{item1} and #{item2}"}
