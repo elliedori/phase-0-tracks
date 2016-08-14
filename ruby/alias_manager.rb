1e23fe07b5d323666ba0715dd74e7a06d3208aad
elliedori <elliedori@gmail.com>
2016-08-13 19:56:21 -0700

    Update alias spy name file with method code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,35 +4,46 @@
 # Map each constonant to the next one
 # Return the result
 
-def spy_namer (name)
-  full_name = name.split(' ')
-
-  first_name = full_name[0]
-  last_name = full_name[1]
-
-  spy name = last_name + ' ' + first_name
-
+def name_scramble(name)
   vowels = "aeiou"
   consonants = "bcdfghjklmnpqrstvwxyz"
 
-raw_spy_name = spy_name.chars.map! do |letter|
-  # case for replacing a vowel
+  spy_name = name.chars.map do |letter|
+
+    #vowel replacement
     if vowels.include? letter
-      current = vowels.index('letter')
-      if current + 1 = vowels.length
+      current = vowels.index(letter)
+      p current
+      #looping back to first vowel in case it's the last one
+      if current + 1 == vowels.length
         letter = vowels[0]
       else
         letter = vowels[current+1]
       end
-  # case for replacing a consonant
+
+    #consonant replacement
     else
-      current = consonants.index('letter')
-      if current + 1 = consonants.length
+      current = consonants.index(letter)
+      # looping back to first consonant in case it's the last one
+      if current + 1 == consonants.length
         letter = consonants[0]
       else
         letter = consonants[current+1]
       end
     end
-  end
+    end
+ spy_name = spy_name.join
+end
 
+
+
+
+def spy_namer(name)
+  full_name = name.split(' ')
+  full_name.map! {|name| name_scramble(name)}
+  full_name[0], full_name[1] = full_name[1], full_name[0]
+  full_name.join(' ')
+end
+
+p spy_namer('britney hodson')
 
