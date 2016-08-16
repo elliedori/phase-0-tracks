974e5f47a5022e7c21fae477228a149def2f7727
elliedori <elliedori@gmail.com>
2016-08-16 16:16:40 -0700

    Update gps file with pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,29 @@
+# Method to create a list
+# create an empty hash
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # separate the string into individual items
+  # add each item to the hash, set default quantity
+  # print the list to the console using a loop
+# output: (hash), could also be a string with hash data inside
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: add the item name and quantity as a new pair into existing hash
+# output: return hash
+
+# Method to remove an item from the list
+# input: using a pre-existing hash method, give it the input of the key of the pair I want to delete
+# steps: the method deletes it
+# output: the updated hash
+
+# Method to update the quantity of an item
+# input: hash of specific item now equals new quantity 
+# steps: the method updates the quantity
+# output: returns the updated hash
+
+# Method to print a list and make it look pretty
+# input: using a loop to iterate through each pair in the hash
+# steps: tell it what to do with each pair (i.e. print it inside of this fancy string)
+# output: each string with interpolated data, return nil?
+
