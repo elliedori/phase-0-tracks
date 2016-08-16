c0c673f665c5d693e7a84c8e275109459c089afc
elliedori <elliedori@gmail.com>
2016-08-16 14:37:49 -0700

    Update santa file to include code for release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,9 @@
 class Santa
-attr_reader :age, :ethnicity
-attr_writer :gender
+
+#changing age & gender to attr_accessor methods to be able to read that data for release 4
+
+attr_reader :ethnicity
+attr_accessor :age, :gender
 
 def initialize(gender, ethnicity)
   puts "Initializing Santa instance..."
@@ -30,15 +33,21 @@ end
 
 end
 
-# santas = []
-# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# driver code for releases 2 & 3
+#  ———————————————————
 
-# 10.times do
-#   santas << Santa.new(example_genders[rand(7)], example_ethnicities[rand(7)])
-# end
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+10.times do
+  santas << Santa.new(example_genders[rand(7)], example_ethnicities[rand(7)])
+end
+
+p santas
 
 santa = Santa.new("agender", "Latino")
+
 p santa
 santa.celebrate_birthday
 santa.get_mad_at("Rudolph")
@@ -47,3 +56,22 @@ santa.gender = "female"
 p santa
 p santa.age
 p santa.ethnicity
+
+#  ———————————————————
+
+#code for release 4
+
+cool_santas = []
+genders = ["agender", "female", "bigender", "male", "female", "not sure", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "Middle Eastern", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+
+200.times do 
+cool_santas << Santa.new(genders[rand(8)], ethnicities[rand(8)])
+end
+
+cool_santas.each do |santa|
+  santa.age = rand(141)
+  puts "This santa is #{santa.gender}, #{santa.ethnicity} and #{santa.age} years old."
+end
+
