848b3c12a993bf522f8d31764082062d4357caed
elliedori <elliedori@gmail.com>
2016-08-16 17:29:09 -0700

    Update gps file with reflection answers

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -3,9 +3,9 @@
 # steps: tell it what to do with each pair (i.e. print it inside of this fancy string)
 # output: each string with interpolated data, return nil?
 
-require "pry"
-
 def list_writer(list)
+  puts "Here's your grocery list for the day:\n"
+  puts "\n"
   list.each do |item, quantity|
     puts "#{item}: #{quantity}"
 end
@@ -33,7 +33,7 @@ end
 # steps: add the item name and quantity as a new pair into existing hash
 # output: return hash
 
-def list_adder(list, item, quantity)
+def list_adder(list, item, quantity = 1)
   list[item] = quantity
 end
 
@@ -43,6 +43,9 @@ end
 # output: the updated hash
 
 def item_remover(list, item)
+  if !list.include? (item)
+    puts "Looks like that's not even on your list! No worries there :)"
+  end
   list.delete(item)
 end
 
@@ -52,27 +55,54 @@ end
 # output: returns the updated hash
 
 def item_updater(list, item, quantity)
-  list[item] = quantity
+  if !list.include? (item)
+    puts "Looks like that item's not on your list! But we'll go ahead and add it for you."
+  end
+    list[item] = quantity
 end
 
 
 # driver code to test methods are working correctly
-grocery_list = {}
-list_creator(grocery_list, "carrots apples cereal pizza")
-list_adder(grocery_list, "potatoes", 6)
-p grocery_list
-item_remover(grocery_list, "apples")
-p grocery_list
-item_updater(grocery_list, "carrots", 3)
-p grocery_list
+new_list = {}
+list_adder(new_list, "lemonade", 2)
+list_adder(new_list, "tomatoes", 3)
+list_adder(new_list, "lemonade", 2)
+list_adder(new_list, "onions", 1)
+list_adder(new_list, "ice cream", 4)
+item_remover(new_list, "lemonade")
+item_updater(new_list, "ice cream", 1)
+item_updater(new_list, "hamburgers", 2)
+list_adder(new_list, "chives")
+list_writer(new_list)
 
+# What did you learn about pseudocode from working on this challenge?
+# I learned that pseudocoding can be helpful in laying out a plan for how to build something.
+# Sometimes I feel like pseudocoding is harder than the actual coding for me, because it makes more
+# sense for me to think about something in terms of Ruby's syntax, rather than explaining what should
+# be done in plain English.
 
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# A hash makes more sense, given the format of item: quantity. One tradeoff is that printing the
+# contents is a little more involved, since it requires a loop that takes two parameters instead of one.
 
+# What does a method return?
+# A method usually returns the result of its last line, unless an explicit return value is given.
 
+# What kind of things can you pass into methods as arguments?
+# You can pass arguments of any type! (i.e. hashes, arrays, numbers, strings, etc.)
+# As long as what you're doing to each argument within the method
+# is valid for that data type, you shouldn't run into problems.
 
+# How can you pass information between methods?
+# You can pass information between methods by keeping the information in a more general scope than each method.
+# For example, here we kept the grocery_list hash at the same scope as the methods, and passed it in
+# as an argument to each method, ensuring each method could modify it as needed.
 
-
-
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# I feel like refactoring for clarity was solidifed for me here, and thinking of new ways to expand
+# methods to do things in a more refined or user-friendly way. It's important to give users explicit 
+# feedback, especially when certain methods aren't printing anything out to the screen. I'm pretty clear
+# on Ruby, no confusing concepts at this point :)
 
 
 
