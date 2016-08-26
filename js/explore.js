cdba599efd67b9d29b0e0ff10fee6131e6b02301
elliedori <elliedori@gmail.com>
2016-08-26 11:04:42 -0700

    Initial commit of explore.js file

---
+++ js/explore.js
@@ -0,0 +1,12 @@
+// take input string
+// create empty output string
+// remove last letter of input string
+// add that letter to empty string
+// keep doing this until the input string is empty
+// return output string
+
+function reverse(str) {
+  var newString = ""
+  for (var i = str.length; i > 0, i--)
+    newString += str[i]
+}
