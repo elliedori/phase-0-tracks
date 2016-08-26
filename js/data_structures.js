822cf31997bd5cafd5b23bbaeb5c24d3a006a7c3
elliedori <elliedori@gmail.com>
2016-08-26 16:46:46 -0700

    Create horse/color object

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,11 @@ var names = ["Dan", "Dave", "Eleanor", "Rupert"]
 
 colors.push("green")
 names.push("Nathaniel")
+
+happyHorses = {}
+
+for (i = 0; i < names.length; i++) {
+  happyHorses[names[i]] = colors[i]
+}
+
+console.log(happyHorses)
