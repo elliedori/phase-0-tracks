c1115f64d64726a1d3e0e96d24166c53b2425a85
elliedori <elliedori@gmail.com>
2016-08-26 17:17:46 -0700

    Add constructor function for Cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,3 +11,47 @@ for (i = 0; i < names.length; i++) {
 }
 
 console.log(happyHorses)
+
+// ————————————————————————————————————————————————————————
+
+function Car(color, manual, make) {
+  console.log("Our new car:", this);
+
+  this.color = color;
+  this.manual = manual;
+  this.make = make;
+
+  this.drive = function() {console.log(this.color + " " + this.make + " went vroom!");};
+
+  console.log("Have fun driving your new car :)")
+}
+
+var firstCar = new Car("black", true, "Honda");
+var secondCar = new Car("green", false, "Toyota")
+var thirdCar = new Car("silver", false, "Lexus")
+
+console.log(Car)
+firstCar.drive();
+thirdCar.drive();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
