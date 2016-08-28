42033859bc156c9871d0df8420f0d71f860fd225
elliedori <elliedori@gmail.com>
2016-08-27 19:36:16 -0700

    Add pseduocode for key value pair algo

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-// find longest phrase
+// LONGEST PHASE ALGO
 // input is array of strings
 // create variable longest, set it equal to empty strings
 // for each string in the array, check to see if its length is longer than longest
@@ -16,7 +16,49 @@ function longestFinder(array) {
   return longest
 }
 
-// driver code to test longestFinder
+// SHARED KEY VALUE PAIR ALGO
+// input is two objects
+// create sameKey object, which is empty
+// create pairMatch variable and set to false
+// for each item in the first object, 
+//   check to see if it matches any of the items in the second object (nested for loop)
+//   if it does, add both objects to sameKey
+// then for each even + odd index in sameKey, check if those keys have the same value
+// if they do change pairMatch to true
+// once a true pair has been found, stop looking
+// return pairMatch
+
+// function pairChecker(firstObj, secondObj) {
+//   sameKey = {};
+//   var pairMatch = false;
+//   for (i = 0; i < firstObj.length; i++) {
+//     for (i = 0; i < secondObj.length; i++) {
+//       if firstObj
+//     };
+
+//   }
+
+
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// ———————————————————————————————————— DRIVER CODE ———————————————————————————————————— //
+
+// testing longest phrase
 longestFinder(["long phrase","longest phrase","longer phrase"])
 longestFinder(["cat", "kitten", "pterodactyl"])
 longestFinder(["is this", "that it is", "that which it is is that"])
+
