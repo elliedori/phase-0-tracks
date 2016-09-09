cf416eaa9f81b1fd2e9d6108a9b2203ac755a79d
elliedori <elliedori@gmail.com>
2016-09-08 17:32:54 -0700

    Add JS code to style site at load

--- web_dev/client_side_js/dom-practice.js
+++ web_dev/client_side_js/dom-practice.js
@@ -2,3 +2,17 @@ var photos = document.getElementsByTagName("img");
 artsyPhoto = photos[0];
 launchPhoto = photos[1];
 
+console.log(artsyPhoto);
+console.log(launchPhoto);
+artsyPhoto.style.border = "10px dotted pink";
+launchPhoto.style.border = "10px yellow";
+launchPhoto.style.borderStyle = "dotted dashed solid double";
+
+var title = document.getElementById("title");
+title.style.fontSize = "5em";
+
+var subtitle = document.getElementById("subtitle");
+subtitle.style.fontSize = "3em";
+
+var body = document.getElementById("body");
+body.style.backgroundColor = "orange";
