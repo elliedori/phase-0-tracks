0899153c5d78e5683ac1da1c94baf011b9b5a391
elliedori <elliedori@gmail.com>
2016-09-10 18:32:57 -0700

    Update app file with /address GET route

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,9 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays address
+
+get '/address' do
+  "633 Folsom St, San Francisco, CA 94107"
+end
